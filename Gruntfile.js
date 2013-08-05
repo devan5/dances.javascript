f280ca5c95ee069e7d27625e3c7679090a2a258c
devan5 <devan5.pan@gmail.com>
2013-08-05 14:03:14 +0800

    Fix:  generator sourcesMap path is confused.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt){
 	var
-        oModule
+        oModule,
+		distpaths = [],
+
+		fs = require( "fs" )
 	;
 
     oModule = {
@@ -8,6 +11,11 @@ module.exports = function(grunt){
 	    srcBase: "snap/",
 		destBase: "dist/"
     };
+
+	distpaths.push(
+		oModule.destBase + oModule.name + "-map.js",
+		oModule.destBase + oModule.name + ".min.js"
+	);
 
 	grunt.initConfig({
 		pkg      : grunt.file.readJSON("package.json"),
@@ -30,18 +38,44 @@ module.exports = function(grunt){
 		uglify   : {
 			generic: {
 				expand : true,
+
 				options: {
-					banner: '/* <%= pkg.name %> - v<%= pkg.version %> - ' +
-					        '<%= grunt.template.today("yyyy-mm-dd, h:MM:ss TT") %> */' +
-					        '\n',
-					report   : "gzip",
-					sourceMap: oModule.destBase + oModule.name + "-map.js"
+					report          : "gzip",
+					sourceMap       : oModule.destBase + oModule.name + "-map.js",
+					sourceMappingURL: oModule.name + "-map.js"
 				},
-				src    : oModule.destBase + oModule.name + ".js",
-//				ext    : ".javascript.min.js"
-				ext    : (oModule.name.slice(oModule.name.lastIndexOf(".") + 1) + ".min.js")
+
+				src: oModule.destBase + oModule.name + ".js",
+				ext: oModule.name.slice(oModule.name.lastIndexOf(".")) + ".min.js"
+			}
+		},
+		clean: {
+			generic: {
+				src: oModule.destBase + oModule.name + "*.js"
 			}
 		}
+	});
+
+	grunt.registerTask( "dist", function() {
+		// TODO 读取目录下的文件, 获取真实文件数量
+		distpaths.forEach(function( filename ) {
+			var
+				text = fs.readFileSync(filename, "utf8")
+			;
+
+			// Modify map/min so that it points to files in the same folder;
+			// see https://github.com/mishoo/UglifyJS2/issues/47
+			if(/-map\.js$/.test(filename)){
+				text = text.replace(/"dist\//g, "\"");
+				fs.writeFileSync(filename, text, "utf-8");
+
+			} else if ( /\.min\.js$/.test( filename ) ) {
+				text += "\n" + "// Build @devan5 at " + grunt.template.today("yyyy-mm-dd, h:MM:ss TT");
+				fs.writeFileSync(filename, text, "utf-8");
+			}
+
+		});
+
 	});
 
 	grunt.loadNpmTasks("grunt-contrib-concat");
@@ -49,5 +83,10 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 
 	grunt.registerTask("default", []);
-	grunt.registerTask("build", ["concat:generic","uglify:generic"]);
+	grunt.registerTask("build", [
+		"clean:generic",
+		"concat:generic",
+		"uglify:generic",
+		"dist"
+	]);
 };
